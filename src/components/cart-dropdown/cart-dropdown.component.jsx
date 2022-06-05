43686283b2d3455b63cc1a7ff5be23d80b07148f
venkanna77 <venkanna948@gmail.com>
2022-06-05 21:01:23 +0530

    cart component changes

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,11 +1,17 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 import { CartContext } from '../usercontext/cart.context';
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = () => {
-    const  {cartItems} = useContext(CartContext)
+    const { cartItems, isCartOpen, setIsCartOpen } = useContext(CartContext);
+    const navigate = useNavigate()
+    const gotoCheckout = () =>{
+      navigate('/checkout');
+      setIsCartOpen(!isCartOpen);
+    }
     return (
       <div className="cart-dropdown-container">
         <div className="cart-items">
@@ -13,7 +19,7 @@ const CartDropdown = () => {
             <CartItem key={item.id} cartItem={item} />
           ))}
         </div>
-        <Button>GO TO CHECKOUT</Button>
+        <Button onClick={gotoCheckout}>GO TO CHECKOUT</Button>
       </div>
     );
 }
